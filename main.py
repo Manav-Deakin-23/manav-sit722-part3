035b1851cefafafd21c9c717e56b5b06453a8d64
Manav Mehta <s223608412@deakin.edu.au>
2024-09-16 01:00:12 +1000

    final commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from . import crud, models, schemas
-from fastapi import FastAPI
+from fastapi import FastAPI 
 from book_catalog.main import app as book_catalog_app
-from inventory_management.main import app as inventory_management_app
+from inventory_management.main import app as inventory_management_app 
 
 app = FastAPI()
 
@@ -10,5 +9,5 @@ app.mount("/book_catalog", book_catalog_app)
 app.mount("/inventory_management", inventory_management_app)
 
 if __name__ == "__main__":
-    import uvicorn
+    import uvicorn 
     uvicorn.run(app, host="0.0.0.0", port=8000)
